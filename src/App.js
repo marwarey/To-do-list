e4a44f9f184e2ae452cd38dd6503d7961b1cca9a
marwarey <marwariahi171@gmail.com>
2021-12-24 19:53:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
 
-function App() {
+class App extends react.Component{
+  state = {todos:[{id:0,task:'eat',done:false},{id:1,task:'sleep',done:false},{id:2,task:'repeat',done:false}]}
+
+  addtodo=()=>{this.setState({ todos:  [...this.state.todos,{id:Math.random(), done:false,task: this.state.text }]    })}
+  deletetodo=(x )=>{this.setState({todos: this.state.todos.filter(el=> el.id !=x )})}
+
+  donetodo=(x )=>{this.setState({todos: this.state.todos.map(el=> el.id==x ? {...el, done:!el.done} : el )})}
+  render (){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='one' >
+      <input onChange={(event)=>this.setState({text:event.target.value})}/>
+      <button   style={{
+                borderRadius:'5%',
+                backgroundColor:'#dc3545',
+                borderColor:'#dc3545' ,
+                color:'white',
+                textAlign: "center",
+                marginLeft:'100px'
+                
+            }}
+      
+      
+      onClick={()=>this.addtodo()}style={{color:'orange',fontWeight:"bold"}}>Add</button>
+      {this.state.todos.map(el=> <div> 
+        <li style={{textDecoration:el.done ?"line-through":null }}>{el.task}</li>
+        <button   style={{
+                borderRadius:'5%',
+                backgroundColor:'#dc3545',
+                borderColor:'#dc3545' ,
+                color:'white',
+                textAlign: "center",
+                marginLeft:'100px'
+                
+            }}
+        
+        onClick={()=>this.deletetodo(el.id)}style={{color:'purple',fontWeight:"bold"}}>Delete</button>
+        <button   style={{
+                borderRadius:'5%',
+                backgroundColor:'#dc3545',
+                borderColor:'#dc3545' ,
+                color:'white',
+                textAlign: "center",
+                marginLeft:'100px'
+                
+            }}
+        
+        onClick={()=>this.donetodo(el.id)}>Done</button>
+        </div>
+        )}
+    
+    
+    
     </div>
-  );
+  )
+
+  }
 }
 
 export default App;
